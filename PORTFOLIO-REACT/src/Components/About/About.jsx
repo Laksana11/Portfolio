020109ef642c0d57d93dbbdcb8ffa5af0592b5b1
Laksana11 <laxsu1198@gmail.com>
2024-12-11 20:18:12 +0530

    changes

--- PORTFOLIO-REACT/src/Components/About/About.jsx
+++ PORTFOLIO-REACT/src/Components/About/About.jsx
@@ -3,7 +3,12 @@ import './About.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+      <div className="about-title">
+        <h1>About me</h1>
+        <img src="" alt="" />
+      </div>
+    </div>
   )
 }
 
