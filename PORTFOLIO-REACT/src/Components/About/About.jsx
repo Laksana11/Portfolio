ebbb2c146617e772370a0cdb64cfd902108b865d
Laksana11 <laxsu1198@gmail.com>
2025-01-15 19:36:26 +0530

    comm

--- PORTFOLIO-REACT/src/Components/About/About.jsx
+++ PORTFOLIO-REACT/src/Components/About/About.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
 
 const About = () => {
   return (
-    <div className='about'>
+    <div className="about">
+      <div className="about-title">
+        <h1>About me</h1>aaaa
+        <img src="" alt="" />
+      </div>
       <div className="about-title">
         <h1>About me</h1>
         <img src="" alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
